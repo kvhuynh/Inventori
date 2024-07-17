43ef43e96b6ca02d4d2647fdaf9d97b7f4bfb169
Kevin Huynh <kvhuynh820@gmail.com>
2024-07-17 13:53:20 -0700

    catchup

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,18 +1,25 @@
-export {}
+export {};
 
-const {
-    createUser
-} = require("../services/user.service");
+const { createUser, loginUser } = require("../services/user.service");
 
 const handleCreateUser = async (req: any, res: any) => {
-    try {
-        const user = await createUser (req.body, res);
-        return res.json(user);
-    } catch (error: any) {
-        return res.status(400).json(error);
-    }
-}
+	try {
+		const user = await createUser(req.body, res);
+		return res.json(user);
+	} catch (error: any) {
+		return res.status(400).json(error);
+	}
+};
+
+const handleLoginUser = async (req: any, res: any) => {
+	try {
+		const user = await loginUser(req.body, res);
+	} catch (error: any) {
+		return res.status(400).json(error);
+	}
+};
 
 module.exports = {
-    handleCreateUser
-}
+	handleCreateUser,
+    handleLoginUser
+};
--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,20 +1,34 @@
-export {}
+export {};
 
 import { User } from "../models/user.model";
+const bcrypt = require("bcrypt");
 
-const createUser = async (data: any, res:any ) => {
-    const user = await User.create(data)
+const createUser = async (data: any, res: any) => {
+	const user = await User.create(data);
 
-    // !!! add authentication with jwt
-    return user;
-}
+	// !!! add authentication with jwt
+	return user;
+};
 
 const loginUser = async (data: any, res: any) => {
-    const user = await User.findOne({where: {email: data.email}});
+	const user = await User.findOne({ where: { email: data.email } });
+	if (user === null) {
+		throw {
+			name: "UserNotFoundError",
+			message: "Invalid credentials",
+		};
+	}
 
-
-}
+	const isPassword = await bcrypt.compare(data.password, user.password);
+	if (!isPassword) {
+		throw {
+            name: "IncorrectPasswordError",
+            message: "Invalid credentials"
+        };  
+	}
+};
 
 module.exports = {
-    createUser
-}
+	createUser,
+	loginUser,
+};
